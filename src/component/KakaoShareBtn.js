d4e702d4b95d5278cbd03ee1727dbb10d55bdb0f
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-05-09 08:23:02 +0900

    kako 링크 공유 기능 추가

--- src/component/KakaoShareBtn.js
+++ src/component/KakaoShareBtn.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
+import KakaoLogo from "../assets/kakaotalk_logo_icon_147272.svg";
 
 const KakaoShareBtn = ({ data }) => {
   const url = "https://localhost:3000/";
   const resultUrl = window.location.href;
-
-  //   useEffect(() => {
-  //     kakaoButton();
-  //   }, []);
 
   const kakaoButton = () => {
     if (window.Kakao) {
@@ -21,12 +18,12 @@ const KakaoShareBtn = ({ data }) => {
         objectType: "feed",
         content: {
           title: "디저트 판별기 결과",
-          description: "나와 가장 잘맞는 디저트는 무엇일까요?",
+          description: `나와 가장 잘맞는 디저트는 ${data.name}입니다.`,
           imageUrl:
             "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
           link: {
-            mobileWebUrl: "https://developers.kakao.com",
-            webUrl: "https://developers.kakao.com",
+            mobileWebUrl: resultUrl,
+            webUrl: resultUrl,
           },
         },
 
@@ -35,7 +32,7 @@ const KakaoShareBtn = ({ data }) => {
             title: "나도 테스트 하러가기",
             link: {
               mobileWebUrl: url,
-              webUrl: "https://developers.kakao.com",
+              webUrl: url,
             },
           },
         ],
@@ -46,21 +43,12 @@ const KakaoShareBtn = ({ data }) => {
   return (
     <>
       <button
-        className="text-gray-500"
+        className="text-gray-500 mr-2"
         onClick={() => {
           kakaoButton();
         }}
       >
-        <svg
-          fill="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-        >
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-        </svg>
+        <img src={KakaoLogo}></img>
       </button>
     </>
   );
