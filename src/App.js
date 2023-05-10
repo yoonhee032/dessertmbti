79fcbd9bc2e98b05c36157b68d25a321549b12d7
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-05-11 00:20:31 +0900

    GA 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Question from "./pages/Question";
 import Result from "./pages/Result";
+import RouterChangeTracker from "./component/RouterChangeTracker";
 import "./App.css";
 
 function App() {
+  
   return (
     <>
+      <RouterChangeTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/question" element={<Question />} />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import ReactGA from "react-ga";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
